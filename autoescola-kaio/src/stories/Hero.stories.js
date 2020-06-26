04558fa10205badf5a0669ffda684eaf7d6e64c3
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-06-25 21:25:10 -0300

    create stories for Hero

---
+++ autoescola-kaio/src/stories/Hero.stories.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Hero from "../components/Hero";
+
+export default {
+  title: "Hero",
+  component: Hero,
+};
+
+export const usage = () => (
+  <Hero title="Ganhe sua liberdade para ir e vir">
+    <p>A auto escola lider em aprovação!</p>
+  </Hero>
+);
+
+export const withList = () => (
+  <Hero title="Ganhe sua liberdade para ir e vir">
+    <ul>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+    </ul>
+  </Hero>
+);
