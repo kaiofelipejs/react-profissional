a4513d9ebe1afd762f09b804d70344b489414c1d
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-06-28 18:14:06 -0300

    update globalStyle

--- autoescola-kaio/src/components/GlobalStyle.js
+++ autoescola-kaio/src/components/GlobalStyle.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 
+import { BreakpointSize, breakAt } from "./Breakpoints";
+
 const GlobalStyle = createGlobalStyle`
   @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&display=swap");
   
@@ -11,12 +13,73 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box; 
   }
 
-  *, *::before, *::after{
+  *, *::before, *::after {
     box-sizing: inherit;
   }
 
   body {
     margin: 0;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    line-height: 1.3;
+    font-weight: 700;
+    letter-spacing: 2px;
+
+    strong {
+      color: #ffc107;
+    }
+  }
+
+  h1 {
+    font-size: 2.5rem;
+
+    ${breakAt(BreakpointSize.lg)} {
+      font-size: 3.75rem;
+    }
+  }
+
+  h2 {
+    font-size: 2rem;
+
+    ${breakAt(BreakpointSize.lg)} {
+      font-size: 3.125rem;
+    }
+  }
+
+  h3 {
+    font-size: 1.9rem;
+
+    ${breakAt(BreakpointSize.lg)} {
+      font-size: 2.5rem;
+    }
+  }
+
+  h4 {
+    font-size: 1.3rem;
+    font-weight: 600;
+
+    ${breakAt(BreakpointSize.lg)} {
+      font-size: 2.125rem;
+    }
+  }
+
+  h5 {
+    font-size: 1.2rem;
+    font-weight: 300;
+
+    ${breakAt(BreakpointSize.lg)} {
+      font-size: 1.5rem;
+    }
+  }
+
+  h6 {
+    font-size: 1.1rem;
+    font-weight: 600;
+
+    ${breakAt(BreakpointSize.lg)} {
+      font-size: 1.25rem;
+    }
   }
 `;
 
