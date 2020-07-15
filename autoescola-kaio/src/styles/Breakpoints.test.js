90baacdaff6c0e598b8f4d19f64b15813a7288ef
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-07-15 20:22:02 -0300

    create tests for Breakpoints

---
+++ autoescola-kaio/src/styles/Breakpoints.test.js
@@ -0,0 +1,10 @@
+import { BreakpointSizes, breakAt } from "./Breakpoints";
+
+test.each([
+  [BreakpointSizes.sm],
+  [BreakpointSizes.md],
+  [BreakpointSizes.lg],
+  [BreakpointSizes.xl],
+])("break at %ipx", (size) => {
+  expect(breakAt(size)).toEqual(`@media (min-width: ${size}px)`);
+});
