c663ecdc93cc8152e99a790cb36e9dae42645edb
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-07-24 23:33:18 -0300

    add theme for all components

--- autoescola-kaio/src/components/Hero.js
+++ autoescola-kaio/src/components/Hero.js
@@ -3,8 +3,6 @@ import PropTypes from "prop-types";
 import styled, { css } from "styled-components";
 import { breakAt, BreakpointSizes } from "styles/Breakpoints";
 
-const colorYellow = "#ffc107";
-
 const Root = styled.div`
   color: #fff;
   padding: 100px 0;
@@ -47,7 +45,7 @@ const Content = styled.div`
   li {
     &::before {
       content: "\\2713\\0020";
-      color: ${colorYellow};
+      color: ${(props) => props.theme.colors.primary.main};
     }
   }
 `;
