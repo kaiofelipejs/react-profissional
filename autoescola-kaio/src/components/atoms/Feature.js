1e10cdcba33507f7e02c796d3bd5e6af7d36f7b2
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-07-26 13:30:43 -0300

    wip - feature component

---
+++ autoescola-kaio/src/components/atoms/Feature.js
@@ -0,0 +1,70 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Root = styled.div`
+  text-align: center;
+
+  & h5 {
+    margin: 16px 0;
+  }
+
+  & p {
+    margin: 0;
+  }
+`;
+
+const getPrimaryColor = ({ theme }) => theme.colors.primary.main;
+
+const IconRoot = styled.div`
+  display: inline-block;
+  width: 116px;
+  height: 116px;
+  padding: 8px;
+  border-radius: 50%;
+
+  &:hover {
+    border: 1px solid ${getPrimaryColor};
+
+    > * {
+      color: #fff;
+      background-color: ${getPrimaryColor};
+    }
+  }
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #e9e9e9;
+  border-radius: 50%;
+  font-size: 32px;
+  width: 100%;
+  height: 100%;
+  color: ${getPrimaryColor};
+`;
+
+const Feature = ({ icon, title, children }) => (
+  <Root>
+    <IconRoot>
+      <IconContainer>{icon}</IconContainer>
+    </IconRoot>
+    <h5>{title}</h5>
+    <div>{children}</div>
+  </Root>
+);
+
+Feature.defaultProps = {
+  icon: undefined,
+  title: "",
+  children: undefined,
+};
+
+Feature.propTypes = {
+  icon: PropTypes.node,
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
+
+export default Feature;
