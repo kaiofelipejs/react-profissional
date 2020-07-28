28d765fe376211ec1b0e559a9e82b057df1e5658
Kaio Felipe Silva <kaiofelipe.js@gmail.com>
2020-07-27 21:38:38 -0300

    create Section and Container components

---
+++ autoescola-kaio/src/components/atoms/Container.js
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+import { breakAt, BreakpointSizes } from "styles/Breakpoints";
+
+const Container = styled.div`
+  width: 100%;
+  padding: 0 8px;
+
+  ${breakAt(BreakpointSizes.sm)} {
+    padding: 0 16px;
+  }
+
+  ${breakAt(BreakpointSizes.lg)} {
+    padding: 0;
+    width: 1140px;
+    margin: 0 auto;
+  }
+`;
+
+Container.defaultProps = {
+  children: undefined,
+};
+
+Container.propTypes = {
+  children: PropTypes.node,
+};
+
+export default Container;
